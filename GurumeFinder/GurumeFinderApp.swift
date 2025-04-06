ea5516a536b0253f67c962f656dc77caadb0e8c6
Ota725 <131368538+Ota725@users.noreply.github.com>
2025-04-06 17:12:52 +0900

    Initial Commit

---
+++ GurumeFinder/GurumeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GurumeFinderApp.swift
+//  GurumeFinder
+//
+//  Created by 太田陽菜 on 2025/04/06.
+//
+
+import SwiftUI
+
+@main
+struct GurumeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
